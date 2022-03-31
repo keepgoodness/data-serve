41a8627de5e14c1cb007903ecc520c08fa6a048c
keepgoodness <yordanlazarov83@gmail.com>
2022-03-31 14:31:08 +0300

    added implementation of UserService interface

--- src/main/java/com/dataserve/service/UserServiceImpl.java
+++ src/main/java/com/dataserve/service/UserServiceImpl.java
@@ -15,18 +15,20 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserserviceImpl implements UserService {
+public class UserServiceImpl implements UserService {
 
 	private final UserRepo userRepo;
 	private final RoleRepo roleRepo;
 
 	@Override
 	public User saveUser(User user) {
+		log.info("Saving new user {} to the database.", user.getName());
 		return userRepo.save(user);
 	}
 
 	@Override
 	public Role saveRole(Role role) {
+		log.info("Saving new role {} to the database.", role.getName());
 		return roleRepo.save(role);
 	}
 
@@ -34,16 +36,19 @@ public class UserserviceImpl implements UserService {
 	public void addRoleToUser(String username, String roleName) {
 		User user = userRepo.findByUsername(username);
 		Role role = roleRepo.findByName(roleName);
+		log.info("Adding role {} to user {}.", roleName, username);
 		user.getRoles().add(role);
 	}
 
 	@Override
 	public User getUser(String username) {
-		return null;
+		log.info("Fetching user {}.", username);
+		return userRepo.findByUsername(username);
 	}
 
 	@Override
 	public List<User> getUsers() {
-		return null;
+		log.info("Fetching all users.");
+		return userRepo.findAll();
 	}
 }
